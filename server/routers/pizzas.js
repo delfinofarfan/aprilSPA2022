f930a71d7d8d7276f262737e4b49444df9e64d5e
Delfino Farfan <delfino.farfan18@gmail.com>
2022-06-01 19:21:10 -0500

    update

--- server/routers/pizzas.js
+++ server/routers/pizzas.js
@@ -59,4 +59,13 @@ router.put("/:id", (request, response) => {
   );
 });
 
+router.get("/:atrib/:value", (request, response) => {
+  let filter = {};
+  filter[request.params.atrib] = request.params.value;
+  Pizza.find(filter, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
 module.exports = router;
